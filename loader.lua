8025c5e167e183d12f2c40cd5bf83dba3f445ad2
Cccccinderela <digitalvaultbrick@gmail.com>
2025-09-05 04:26:23 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,16 +1,1 @@
-local prospecting = 129827112113663
-local nightsinforest = 126509999114328
-local sba1 = 109983668079237
-local sba2 = 96342491571673
-
-if game.PlaceId == prospecting then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/frequencylol/111222/refs/heads/main/prospecting"))()
-elseif game.PlaceId == nightsinforest then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/frequencylol/divineloader/refs/heads/main/99nights.txt"))()
-elseif game.PlaceId == sba1 or game.PlaceId == sba2 then
-     loadstring(game:HttpGet("https://raw.githubusercontent.com/frequencylol/divineloader/refs/heads/main/sba.lua"))()
-else
-    print("No matching Game ID. Current PlaceId: " .. game.PlaceId)
-end
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/frequencylol/divineloader/refs/heads/main/nun.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/972dc38f8123bffad5597decd0a6d7c8.lua"))()
