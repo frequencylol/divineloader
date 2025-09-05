ba7fef7a9a15ba508fa8f6e59e5b40e107cc60b0
Cccccinderela <digitalvaultbrick@gmail.com>
2025-09-05 04:32:45 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,3 @@
 setclipboard('loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/972dc38f8123bffad5597decd0a6d7c8.lua"))()')
+wait(0.5)
 game.Players.LocalPlayer:Kick("Script copied to clipboard, re execute")
